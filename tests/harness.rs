948f2fe96c2e94afc0384c88886cceb0c9e9160d
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2022-01-20 08:31:27 -0700

    Fill out test

--- tests/harness.rs
+++ tests/harness.rs
@@ -1,6 +1,31 @@
+use fuel_tx::Salt;
+use fuels_abigen_macro::abigen;
+use fuels_rs::contract::Contract;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 
+abigen!(MyContract, "./BentoBoxABI.json");
 
 #[tokio::test]
 async fn harness() {
-    assert_eq!(true, true);
+    let rng = &mut StdRng::seed_from_u64(2322u64);
+
+    // Build the contract
+    let salt: [u8; 32] = rng.gen();
+    let salt = Salt::from(salt);
+    let compiled = Contract::compile_sway_contract("./", salt).unwrap();
+
+    // Launch a local network and deploy the contract
+    let (client, _contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
+
+    let contract_instance = MyContract::new(compiled, client);
+
+    // Call `initialize_counter()` method in our deployed contract.
+    // Note that, here, you get type-safety for free!
+    let _result = contract_instance
+        .deposit(1000000, 100, [0; 32], 100)
+        .call()
+        .await
+        .unwrap();
+
 }
