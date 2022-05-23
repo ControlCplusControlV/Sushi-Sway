8ec0b9238e777112a10ac78679ce3ba9f0978ecd
Johann <randomlikeabledude@gmail.com>
2022-05-22 18:31:19 -0600

    Maybe updated repo?

--- tests/harness.rs
+++ tests/harness.rs
@@ -1,36 +1,42 @@
-use fuel_tx::Salt;
+use fuel_tx::ContractId;
+use fuels::prelude::*;
+use fuels::test_helpers;
 use fuels_abigen_macro::abigen;
-use fuels_rs::contract::Contract;
-use rand::rngs::StdRng;
-use rand::{Rng, SeedableRng};
 
-abigen!(MyContract, "./BentoBoxABI.json");
+// Load abi from json
+abigen!(MyContract, "out/debug/BentoBox.json");
 
-#[tokio::test]
-async fn deposit() {
-    let rng = &mut StdRng::seed_from_u64(2322u64);
-
-    // Build the contract
-    let salt: [u8; 32] = rng.gen();
-    let salt = Salt::from(salt);
-    let compiled = Contract::compile_sway_contract("./", salt).unwrap();
+async fn get_contract_instance() -> (MyContract, ContractId) {
+    // Deploy the compiled contract
+    let compiled = Contract::load_sway_contract("./out/debug/BentoBox.bin").unwrap();
 
     // Launch a local network and deploy the contract
-    let (client, _contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
+    let (provider, wallet) = test_helpers::setup_test_provider_and_wallet().await;
 
-    let contract_instance = MyContract::new(compiled, client);
+    let id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
+        .await
+        .unwrap();
 
-    let input_token_input:[u8; 32] = [0; 32];
-    
-    let from_input:[u8; 32] = [5; 32];
+    let instance = MyContract::new(id.to_string(), provider, wallet);
 
-    let to_input:[u8; 32] = [7; 32];
+    (instance, id)
+}
+
+#[tokio::test]
+async fn test_deposit() {
+    let (contract_instance, _id) = get_contract_instance().await;
+
+    let input_token_input: [u8; 32] = [0; 32];
+
+    let from_input: [u8; 32] = [5; 32];
+
+    let to_input: [u8; 32] = [7; 32];
 
     let input_struct = mycontract_mod::DepositInput {
-        from: from_input, 
+        from: from_input,
         to: to_input,
         amount: 10000000000,
-        asset_id: [0; 32]
+        asset_id: [0; 32],
     };
 
     // Call `initialize_counter()` method in our deployed contract.
@@ -42,33 +48,24 @@ async fn deposit() {
         .unwrap();
 
     assert!(result > 1);
+    // Now you have an instance of your contract you can use to test each function
 }
 
 #[tokio::test]
-async fn balance_of() {
-    let rng = &mut StdRng::seed_from_u64(2322u64);
+async fn test_withdraw() {
+    let (contract_instance, _id) = get_contract_instance().await;
 
-    // Build the contract
-    let salt: [u8; 32] = rng.gen();
-    let salt = Salt::from(salt);
-    let compiled = Contract::compile_sway_contract("./", salt).unwrap();
+    let input_token_input: [u8; 32] = [0; 32];
 
-    // Launch a local network and deploy the contract
-    let (client, _contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
+    let from_input: [u8; 32] = [5; 32];
 
-    let contract_instance = MyContract::new(compiled, client);
-
-    let input_token_input:[u8; 32] = [0; 32];
-    
-    let from_input:[u8; 32] = [5; 32];
-
-    let to_input:[u8; 32] = [7; 32];
+    let to_input: [u8; 32] = [7; 32];
 
     let input_struct = mycontract_mod::DepositInput {
-        from: from_input, 
+        from: from_input,
         to: to_input,
         amount: 10000000000,
-        asset_id: [0; 32]
+        asset_id: [0; 32],
     };
 
     // Call `initialize_counter()` method in our deployed contract.
@@ -78,13 +75,12 @@ async fn balance_of() {
         .call()
         .await
         .unwrap();
-    
+
     let balance_of_check = mycontract_mod::BalanceOfInput {
         address: to_input,
         asset_id: [0; 32],
-        salt: 10
+        salt: 10,
     };
-
 
     let result = contract_instance
         .balance_of(balance_of_check)
@@ -96,30 +92,20 @@ async fn balance_of() {
 }
 
 #[tokio::test]
-async fn transfer() {
-    let rng = &mut StdRng::seed_from_u64(2322u64);
+async fn test_transfer() {
+    let (contract_instance, _id) = get_contract_instance().await;
 
-    // Build the contract
-    let salt: [u8; 32] = rng.gen();
-    let salt = Salt::from(salt);
-    let compiled = Contract::compile_sway_contract("./", salt).unwrap();
+    let input_token_input: [u8; 32] = [0; 32];
 
-    // Launch a local network and deploy the contract
-    let (client, _contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
+    let from_input: [u8; 32] = [5; 32];
 
-    let contract_instance = MyContract::new(compiled, client);
-
-    let input_token_input:[u8; 32] = [0; 32];
-    
-    let from_input:[u8; 32] = [5; 32];
-
-    let to_input:[u8; 32] = [7; 32];
+    let to_input: [u8; 32] = [7; 32];
 
     let input_struct = mycontract_mod::DepositInput {
-        from: from_input, 
+        from: from_input,
         to: to_input,
         amount: 10000000000,
-        asset_id: [0; 32]
+        asset_id: [0; 32],
     };
 
     // Call `initialize_counter()` method in our deployed contract.
@@ -129,11 +115,11 @@ async fn transfer() {
         .call()
         .await
         .unwrap();
-    
+
     let balance_of_check = mycontract_mod::BalanceOfInput {
         address: [10; 32],
         asset_id: [0; 32],
-        salt: 1
+        salt: 1,
     };
 
     let transfer_check = mycontract_mod::TransferInput {
@@ -146,7 +132,7 @@ async fn transfer() {
     let balance_of_check2 = mycontract_mod::BalanceOfInput {
         address: [10; 32],
         asset_id: [0; 32],
-        salt: 2
+        salt: 2,
     };
 
     let result3 = contract_instance
@@ -165,30 +151,20 @@ async fn transfer() {
 }
 
 #[tokio::test]
-async fn withdraw() {
-    let rng = &mut StdRng::seed_from_u64(2322u64);
+async fn test_balanceOf() {
+    (contract_instance, _id) = get_contract_instance().await;
 
-    // Build the contract
-    let salt: [u8; 32] = rng.gen();
-    let salt = Salt::from(salt);
-    let compiled = Contract::compile_sway_contract("./", salt).unwrap();
+    let input_token_input: [u8; 32] = [0; 32];
 
-    // Launch a local network and deploy the contract
-    let (client, _contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
+    let from_input: [u8; 32] = [5; 32];
 
-    let contract_instance = MyContract::new(compiled, client);
-
-    let input_token_input:[u8; 32] = [0; 32];
-    
-    let from_input:[u8; 32] = [5; 32];
-
-    let to_input:[u8; 32] = [7; 32];
+    let to_input: [u8; 32] = [7; 32];
 
     let input_struct = mycontract_mod::DepositInput {
-        from: from_input, 
+        from: from_input,
         to: to_input,
         amount: 10000000000,
-        asset_id: [0; 32]
+        asset_id: [0; 32],
     };
 
     // Call `initialize_counter()` method in our deployed contract.
@@ -198,50 +174,18 @@ async fn withdraw() {
         .call()
         .await
         .unwrap();
-    
+
     let balance_of_check = mycontract_mod::BalanceOfInput {
         address: to_input,
         asset_id: [0; 32],
-        salt: 7
+        salt: 10,
     };
 
-
-    let initial_bal = contract_instance
+    let result = contract_instance
         .balance_of(balance_of_check)
         .call()
         .await
         .unwrap();
 
-    let withdraw_check = mycontract_mod::WithdrawInput {
-        from: to_input, 
-        to: to_input,
-        asset_id: [0; 32],
-        amount: 1000
-    };
-
-    let balance_of_check2 = mycontract_mod::BalanceOfInput {
-        address: [10; 32],
-        asset_id: [0; 32],
-        salt: 10,
-    };
-
-    let result3 = contract_instance
-        .withdraw(withdraw_check)
-        .call()
-        .await
-        .unwrap();
-
-    let balance_of_check1 = mycontract_mod::BalanceOfInput {
-        address: [10; 32],
-        asset_id: [0; 32],
-        salt: 140
-    };
-
-    let final_bal = contract_instance
-        .balance_of(balance_of_check1)
-        .call()
-        .await
-        .unwrap();
-    
-    assert!(initial_bal > final_bal);
+    assert!(result > 1);
 }
